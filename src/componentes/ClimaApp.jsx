05197e84c77f5d6072325e5e51678fc04433e87e
ErwinAdriel <adrielquispe87@gmail.com>
2024-05-23 22:20:46 -0300

    modificando title usando useEffect

--- src/componentes/ClimaApp.jsx
+++ src/componentes/ClimaApp.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ClimaForm from "./ClimaForm";
 export default function ClimaApp(){
 
     const [weather, setWeather] = useState(null);
+
+    useEffect(() => {
+        loadInfo();
+    }, []);
+
+    useEffect(() => {
+        document.title = `Clima App | ${weather?.location.name ?? ""}`;
+    }, [weather]);
 
     async function loadInfo(city = 'london'){
         try{
@@ -28,8 +36,25 @@ export default function ClimaApp(){
 
     return(
         <div>
+            <h1>ClimaApp</h1>
             <ClimaForm onChangeCity={handleChangeCity} />
-            <div>{weather?.current.temp_c}</div>
+            <div>
+                <div className="imgIcon">
+                    {weather?.current.condition.icon}
+                </div>
+                <div className="clima">
+                    {weather?.current.condition.text}
+                </div>
+                <div className="temperatura">
+                    {weather?.current.temp_c}
+                </div>
+                <div className="humedad">
+                    {weather?.current.humidity}
+                </div>
+                <div className="ciudadPais">
+                    {weather?.location.name}, {weather?.location.country}
+                </div>
+            </div>
         </div>
     );
 }
